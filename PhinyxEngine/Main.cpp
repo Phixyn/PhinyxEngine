5dc60305f368cf649c7a4375006efc98a796e7d8
phixyn <phixyn@gmail.com>
2017-03-13 00:11:42 +0000

    chore: revert wrong content on Main.cpp due to mistake in moving repos

--- PhinyxEngine/Main.cpp
+++ PhinyxEngine/Main.cpp
@@ -1,57 +1,16 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
-
-const int VELOCITY = 13;
-
-void moveObject(sf::RectangleShape &object, int maxX, int maxY, int posX, int posY) {
-
-}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Phinyx Engine Demo");
-	sf::RectangleShape shape(sf::Vector2f(40, 80));
-	shape.setFillColor(sf::Color::Blue);
-
-	std::cout << window.getSize().x << std::endl;
-	std::cout << window.getSize().y << std::endl;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Test");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::KeyPressed)
-			{
-				if (event.key.code == sf::Keyboard::W)
-				{
-					if (shape.getPosition().y > 0) {
-						shape.setPosition(shape.getPosition().x, shape.getPosition().y - VELOCITY);
-					}
-				}
-				if (event.key.code == sf::Keyboard::A)
-				{
-					if (shape.getPosition().x > 0) {
-						shape.setPosition(shape.getPosition().x - VELOCITY, shape.getPosition().y);
-					}
-				}
-				if (event.key.code == sf::Keyboard::S)
-				{
-					if (shape.getPosition().y < window.getSize().y - shape.getSize().y) {
-						shape.setPosition(shape.getPosition().x, shape.getPosition().y + VELOCITY);
-					}
-				}
-				if (event.key.code == sf::Keyboard::D)
-				{
-					if (shape.getPosition().x < window.getSize().x - shape.getSize().x) {
-						shape.setPosition(shape.getPosition().x + VELOCITY, shape.getPosition().y);
-					}
-					else if (shape.getPosition().x + VELOCITY > window.getSize().x - shape.getSize().x) {
-						shape.setPosition(window.getSize().x - shape.getSize().x, shape.getPosition().y);
-					}
-				}
-			}
-
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
