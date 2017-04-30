0a303ea6e956a90973f00b62818066e50e98a116
phixyn <phixyn@gmail.com>
2017-04-30 23:49:27 +0100

    fix(Logger): fix timestamp formatting

--- PhinyxEngine/src/Logger.cpp
+++ PhinyxEngine/src/Logger.cpp
@@ -10,7 +10,7 @@ void PhinyxEngine::Logger::log(std::string level, std::string message) {
 	std::ostringstream oss;
 	
 	if (m_showTimestamps == true) {
-		oss << std::put_time(m_timeInfo, "[%d-%m-%Y %H-%M-%S]");
+		oss << std::put_time(m_timeInfo, "[%d-%m-%Y %H:%M:%S]");
 	}
 
 	oss << "[" << level << "]: " << message << std::endl;
