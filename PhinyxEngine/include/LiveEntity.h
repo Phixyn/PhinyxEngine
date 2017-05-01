03c13dd80a3cbb354dc2e372a1444c6f99bdccb6
phixyn <phixyn@gmail.com>
2017-05-01 23:18:10 +0100

    feat(LiveEntity): implement onCollision method

--- PhinyxEngine/include/LiveEntity.h
+++ PhinyxEngine/include/LiveEntity.h
@@ -2,30 +2,38 @@
 #define LIVE_ENTITY_H
 
 #include "Entity.h"
+#include "Logger.h"
 
 namespace PhinyxEngine {
 	class LiveEntity : public Entity {
 		public:
 			LiveEntity(int health, int attackPower);
-			LiveEntity(int health, int attackPower, float speed);
+			LiveEntity(int health, int attackPower, float speed, float jumpHeight);
 			void handleEvents() override;
 			void update(float deltaTime) override;
 			void draw() override;
+
 			// TODO: animate?
+			void onCollision(sf::Vector2f direction);
 
 			bool isAggressive();
 			bool isDead();
 			void setDead(bool dead);
-			float getDirection();
-			void setDirection(float direction);
+
+			// TODO: Direction handling
+			sf::Vector2f getDirection();
+			void setDirection(sf::Vector2f direction);
 		protected:
 			int m_health;
 			int m_attackPower;
 			float m_speed;
-			float m_direction = 1.0f;
-			sf::Vector2f m_acceleration;
+			float m_jumpHeight;
 			bool m_aggressive;
 			bool m_dead;
+			bool m_canJump = true;
+			sf::Vector2f m_velocity;
+			// sf::Vector2f m_direction;
+			Logger m_logger;
 	};
 }
 
