fdad8caaf1538fd577d664c2a40f41fc103eaa8a
phixyn <phixyn@gmail.com>
2017-05-01 17:13:31 +0100

    feat(Player): update update() method to handle player movement based on delta time

--- PhinyxEngine/src/Player.cpp
+++ PhinyxEngine/src/Player.cpp
@@ -1,27 +1,40 @@
 #include "../include/Player.h"
 
-PhinyxEngine::Player::Player(int health, int attackPower) : LiveEntity(health, attackPower) {
+PhinyxEngine::Player::Player(int health, int attackPower, float speed) : LiveEntity(health, attackPower, speed) {
 	m_rectWidth = 48.0f;
 	m_rectHeight = 52.0f;
 	m_rect = sf::RectangleShape(sf::Vector2f(m_rectWidth, m_rectHeight));
 }
 
-
+/*
 void PhinyxEngine::Player::handleEvents() {
+
+}
+*/
+
+void PhinyxEngine::Player::update(float deltaTime) {
+	// Reset acceleration to stop movement
+	m_acceleration.x = 0.0f;
+
 	// TODO: Improve this to take a mapping of keybinds set in the game
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-		m_rect.move(-0.1f, 0.0f);
+		if (m_direction == 1.0f) {
+			m_direction = -abs(m_direction);
+		}
+
+		m_acceleration.x -= m_speed * deltaTime;
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-		m_rect.move(0.1f, 0.0f);
+		if (m_direction == -1.0f) {
+			m_direction = -abs(m_direction);
+		}
+
+		m_acceleration.x += m_speed * deltaTime;
 	}
 
+	m_rect.move(m_acceleration);
 	// TODO: Jump
-}
-
-void PhinyxEngine::Player::update(float deltaTime) {
-	// TODO
 }
 
 void PhinyxEngine::Player::draw() {
