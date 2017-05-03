e6d615effb2b820d70de67706b74a1c386e9a516
phixyn <phixyn@gmail.com>
2017-05-03 23:44:14 +0100

    feat(ExampleGame): add background image

--- ExampleGame/Main.cpp
+++ ExampleGame/Main.cpp
@@ -4,6 +4,6 @@
 
 int main() {
 	PhinyxEngine::Game game(800, 600, "Phinyx Engine Demo", false);
-	game.addScene(std::make_unique<PhinyxEngine::LevelScene>(game, "example.lvl", "example.dat"));
+	game.addScene(std::make_unique<PhinyxEngine::LevelScene>(game, "example.lvl", "example.dat", "background.png"));
 	game.mainLoop();
 }
