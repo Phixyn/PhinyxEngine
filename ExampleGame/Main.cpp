9bd21f9e1dd9524d2d4fc851c68156e8fc5730f0
phixyn <phixyn@gmail.com>
2017-05-04 00:19:53 +0100

    chore: move Main.cpp to source folder

--- ExampleGame/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Game.hpp"
-#include "LevelScene.hpp"
-#include <memory>
-
-int main() {
-	PhinyxEngine::Game game(800, 600, "Phinyx Engine Demo", false);
-	game.addScene(std::make_unique<PhinyxEngine::LevelScene>(game, "example.lvl", "example.dat", "background.png"));
-	game.mainLoop();
-}
