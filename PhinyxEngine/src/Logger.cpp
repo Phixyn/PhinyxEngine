85ee3372a21f3de541e7c1c861cd873fd41c4e27
phixyn <phixyn@gmail.com>
2018-07-22 18:29:06 +0100

    feat(Logger): update Logger class with improvements and fixes

--- PhinyxEngine/src/Logger.cpp
+++ PhinyxEngine/src/Logger.cpp
@@ -2,7 +2,6 @@
 #include <sstream>
 #include <iostream>
 
-/// <summary> Sets the m_showTimestamps member variable </summary>
 PhinyxEngine::Logger::Logger(bool showTimestamps) :
 	m_showTimestamps(showTimestamps)
 {
@@ -12,9 +11,12 @@ PhinyxEngine::Logger::Logger(bool showTimestamps) :
 void PhinyxEngine::Logger::log(std::string level, std::string message)
 {
 	std::ostringstream oss;
-	
+
+	std::time_t m_timeT = std::time(nullptr);
+	errno_t m_localtime = localtime_s(&m_timeInfo, &m_timeT);
+
 	if (m_showTimestamps == true)
-		oss << std::put_time(m_timeInfo, "[%d-%m-%Y %H:%M:%S]");
+		oss << std::put_time(&m_timeInfo, "[%d-%m-%Y %H:%M:%S]");
 
 	oss << "[" << level << "]: " << message << std::endl;
 	std::cout << oss.str();
