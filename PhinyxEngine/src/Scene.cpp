69b87f3e9baa23d6b1f17a32094bb354de65a16b
phixyn <phixyn@gmail.com>
2018-08-05 18:49:23 +0100

    docs(*Scene): update code documentation for scene classes

--- PhinyxEngine/src/Scene.cpp
+++ PhinyxEngine/src/Scene.cpp
@@ -4,7 +4,8 @@
 /// Sets <see cref="m_game_ptr">m_game_ptr</see> to the reference of Game in
 /// the parameter.
 /// </summary>
-PhinyxEngine::Scene::Scene(Game &game) : m_game_ptr(&game)
+PhinyxEngine::Scene::Scene(Game &game) :
+	m_game_ptr(&game)
 {
 
 }
